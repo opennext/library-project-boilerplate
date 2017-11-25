c80a3c3f9312d449e404900c1ae5ec903fcf1989
opennext <opennext@126.com>
2017-11-25 11:04:48 +0800

    yarn install

---
+++ dist/image-util.js
@@ -0,0 +1,20 @@
+var imageUtil = (function () {
+'use strict';
+
+var CONSTANT = {
+  SIZE: 'size',
+  POSITION: 'position'
+};
+
+var resize = function resize() {
+  consle.log('function named resize.');
+};
+
+var imageUtil$1 = {
+  resize: resize,
+  CONSTANT: CONSTANT
+};
+
+return imageUtil$1;
+
+}());
---
+++ es/moduleA/index.js
@@ -0,0 +1,15 @@
+const CONSTANT = {
+  SIZE: 'size',
+  POSITION: 'position'
+};
+
+const resize = () => {
+  consle.log('function named resize.');
+};
+
+var imageUtil = {
+  resize,
+  CONSTANT
+};
+
+export default imageUtil;
